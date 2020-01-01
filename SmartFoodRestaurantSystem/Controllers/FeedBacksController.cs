b513612cd86631c2a2ee23feee74109b9c5506e9
user <user@DESKTOP-SLUGB16>
2019-12-31 22:46:28 -0800

    Add FeedBack Controller and Views

---
+++ SmartFoodRestaurantSystem/Controllers/FeedBacksController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SmartFoodRestaurantSystem.Models;
+
+namespace SmartFoodRestaurantSystem.Controllers
+{
+    public class FeedBacksController : Controller
+    {
+        private readonly SmartFoodResturantContext _context;
+
+        public FeedBacksController(SmartFoodResturantContext context)
+        {
+            _context = context;
+        }
+
+        // GET: FeedBacks
+        public async Task<IActionResult> Index()
+        {
+            var smartFoodResturantContext = _context.FeedBack.Include(f => f.Customer);
+            return View(await smartFoodResturantContext.ToListAsync());
+        }
+
+        // GET: FeedBacks/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var feedBack = await _context.FeedBack
+                .Include(f => f.Customer)
+                .FirstOrDefaultAsync(m => m.FeedBackId == id);
+            if (feedBack == null)
+            {
+                return NotFound();
+            }
+
+            return View(feedBack);
+        }
+
+        // GET: FeedBacks/Create
+        public IActionResult Create()
+        {
+            ViewData["CustomerId"] = new SelectList(_context.CustomerRegistration, "CustomerId", "CustomerId");
+            return View();
+        }
+
+        // POST: FeedBacks/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("FeedBackId,FeedBackDescription,FeedBackEnvironment,FeedBackStaffBehaviour,CustomerId")] FeedBack feedBack)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(feedBack);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerId"] = new SelectList(_context.CustomerRegistration, "CustomerId", "CustomerId", feedBack.CustomerId);
+            return View(feedBack);
+        }
+
+        // GET: FeedBacks/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var feedBack = await _context.FeedBack.FindAsync(id);
+            if (feedBack == null)
+            {
+                return NotFound();
+            }
+            ViewData["CustomerId"] = new SelectList(_context.CustomerRegistration, "CustomerId", "CustomerId", feedBack.CustomerId);
+            return View(feedBack);
+        }
+
+        // POST: FeedBacks/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("FeedBackId,FeedBackDescription,FeedBackEnvironment,FeedBackStaffBehaviour,CustomerId")] FeedBack feedBack)
+        {
+            if (id != feedBack.FeedBackId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(feedBack);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FeedBackExists(feedBack.FeedBackId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerId"] = new SelectList(_context.CustomerRegistration, "CustomerId", "CustomerId", feedBack.CustomerId);
+            return View(feedBack);
+        }
+
+        // GET: FeedBacks/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var feedBack = await _context.FeedBack
+                .Include(f => f.Customer)
+                .FirstOrDefaultAsync(m => m.FeedBackId == id);
+            if (feedBack == null)
+            {
+                return NotFound();
+            }
+
+            return View(feedBack);
+        }
+
+        // POST: FeedBacks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var feedBack = await _context.FeedBack.FindAsync(id);
+            _context.FeedBack.Remove(feedBack);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FeedBackExists(int id)
+        {
+            return _context.FeedBack.Any(e => e.FeedBackId == id);
+        }
+    }
+}

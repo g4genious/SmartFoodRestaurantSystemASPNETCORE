29e9bcc2857bca2f2a56eda2ded31905791f8146
user <user@DESKTOP-SLUGB16>
2019-12-31 20:27:22 -0800

    Configure ORM

--- SmartFoodRestaurantSystem/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
+++ SmartFoodRestaurantSystem/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace SmartFoodRestaurantSystem.Data.Migrations
 {
-    [DbContext(typeof(ApplicationDbContext))]
+    [DbContext(typeof(SmartFoodRestaurantSystemContext))]
     [Migration("00000000000000_CreateIdentitySchema")]
     partial class CreateIdentitySchema
     {
--- SmartFoodRestaurantSystem/Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ SmartFoodRestaurantSystem/Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace SmartFoodRestaurantSystem.Data.Migrations
 {
-    [DbContext(typeof(ApplicationDbContext))]
+    [DbContext(typeof(SmartFoodRestaurantSystemContext))]
     partial class ApplicationDbContextModelSnapshot : ModelSnapshot
     {
         protected override void BuildModel(ModelBuilder modelBuilder)
